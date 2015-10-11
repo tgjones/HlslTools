008c304085ec5f681ae9495cf525a157e6c16cf7
Tim Jones <tim@timjones.tw>
2015-10-11 23:38:38 +0800

    Dispose BackgroundParser when view is closed

---
+++ src/HlslTools.VisualStudio/Parsing/BackgroundParserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using HlslTools.VisualStudio.Util.Extensions;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace HlslTools.VisualStudio.Parsing
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType(HlslConstants.ContentTypeName)]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    internal sealed class BackgroundParserManager : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            // Ensure BackgroundParser is created.
+            textView.TextBuffer.GetBackgroundParser();
+            textView.Closed += OnViewClosed;
+        }
+
+        private static void OnViewClosed(object sender, EventArgs e)
+        {
+            var view = (IWpfTextView)sender;
+            view.Closed -= OnViewClosed;
+
+            var backgroundParser = view.TextBuffer.GetBackgroundParser();
+            backgroundParser?.Dispose();
+        }
+    }
+}
