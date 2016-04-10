d4e721ecce3de48f6618eedbafce431567f0d067
Tim Jones <tim@timjones.tw>
2016-04-10 19:23:18 +0800

    Don't show signature help for function while in nested function call

--- src/HlslTools.VisualStudio/IntelliSense/SignatureHelp/SignatureHelpExtensions.cs
+++ src/HlslTools.VisualStudio/IntelliSense/SignatureHelp/SignatureHelpExtensions.cs
@@ -18,10 +18,15 @@ namespace HlslTools.VisualStudio.IntelliSense.SignatureHelp
             if (argumentList != null)
                 return argumentList.IsBetweenParentheses(position);
 
+            // If there is a nested ArgumentList that contains this position, don't show signature help for this node type.
+            if (node.DescendantNodes().Any(x => x.Kind == SyntaxKind.ArgumentList && x.IsBetweenParentheses(position)))
+                return false;
+
             var leftParenthesisToken = node.GetSingleChildToken(SyntaxKind.OpenParenToken);
             var rightParenthesisToken = node.GetSingleChildToken(SyntaxKind.CloseParenToken);
             var start = leftParenthesisToken.IsMissing ? leftParenthesisToken.SourceRange.Start : leftParenthesisToken.SourceRange.End;
             var end = rightParenthesisToken.IsMissing ? node.FullSourceRange.End : rightParenthesisToken.SourceRange.Start;
+            
             return start <= position && position <= end;
         }
 
