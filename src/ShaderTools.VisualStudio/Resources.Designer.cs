bc63656d813adc8f5bc324d81075876de588e917
Tim Jones <tim@timjones.io>
2016-12-07 18:41:00 +0800

    Update to VS2017 vsix

--- src/ShaderTools.VisualStudio/Resources.Designer.cs
+++ src/ShaderTools.VisualStudio/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace ShaderTools.VisualStudio {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
