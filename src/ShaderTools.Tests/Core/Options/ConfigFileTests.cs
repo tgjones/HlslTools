d801c7ee927284fceb4d220ee0d0b644778476e6
Tim Jones <tim@timjones.io>
2017-01-16 22:25:07 +0800

    Add ConfigFile test

--- src/ShaderTools.Tests/Core/Options/ConfigFileTests.cs
+++ src/ShaderTools.Tests/Core/Options/ConfigFileTests.cs
@@ -1,6 +1,47 @@
-﻿namespace ShaderTools.Tests.Core.Options
+﻿using System.Collections.Generic;
+using System.IO;
+using ShaderTools.Core.Options;
+using Xunit;
+
+namespace ShaderTools.Tests.Core.Options
 {
     public class ConfigFileTests
     {
+        [Fact]
+        public void CanLoadConfigFile()
+        {
+            var configFile = ConfigFileLoader.LoadAndMergeConfigFile(@"ChildFolder\GrandchildFolder");
+
+            Assert.Equal(
+                new Dictionary<string, string>
+                {
+                    { "MY_DEFINE", "1" },
+                    { "MY_OTHER_DEFINE", "2" }
+                }, 
+                configFile.HlslPreprocessorDefinitions);
+
+            Assert.Equal(
+                new string[]
+                {
+                    "../Bar",
+                    "..\\Foo",
+                    ".",
+                    "..",
+                    "Foo"
+                },
+                configFile.HlslAdditionalIncludeDirectories);
+
+            var workingDirectory = Path.GetFullPath(".");
+            Assert.Equal(
+                new string[]
+                {
+                    Path.Combine(workingDirectory, "ChildFolder", "Bar"),
+                    Path.Combine(workingDirectory, "ChildFolder", "Foo"),
+                    Path.Combine(workingDirectory, "ChildFolder"),
+                    workingDirectory,
+                    Path.Combine(workingDirectory, "ChildFolder", "Foo"),
+                },
+                configFile.GetAbsoluteHlslAdditionalIncludeDirectories());
+        }
     }
 }
