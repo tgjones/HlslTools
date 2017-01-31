tgjones/HlslTools

commit 9b2129048d6849840aea5dad5e755c94406dd98c

Bind function/method symbols even when overload resolution fails.
Fixes #71.

src/ShaderTools/Hlsl/Binding/Binder.Expressions.cs
@@ -447,13 +447,22 @@ namespace ShaderTools.Hlsl.Binding
             {
                 if (result.Selected == null)
                 {
-                    Diagnostics.ReportUndeclaredMethod(syntax, target.Type, argumentTypes);
-                    return new BoundErrorExpression();
+                    if (result.Candidates.IsEmpty)
+                    {
+                        Diagnostics.ReportUndeclaredMethod(syntax, target.Type, argumentTypes);
+                        return new BoundErrorExpression();
+                    }
+                    else
+                    {
+                        Diagnostics.ReportOverloadResolutionFailure(syntax, arguments.Length);
+                    }
                 }
-
-                var symbol1 = result.Selected.Signature.Symbol;
-                var symbol2 = result.Candidates.First(c => !c.Signature.Symbol.Equals(symbol1)).Signature.Symbol;
-                Diagnostics.ReportAmbiguousInvocation(syntax.SourceRange, symbol1, symbol2, argumentTypes);
+                else
+                {
+                    var symbol1 = result.Selected.Signature.Symbol;
+                    var symbol2 = result.Candidates.First(c => !c.Signature.Symbol.Equals(symbol1)).Signature.Symbol;
+                    Diagnostics.ReportAmbiguousInvocation(syntax.SourceRange, symbol1, symbol2, argumentTypes);
+                }
             }
 
             // Convert all arguments (if necessary)
@@ -501,13 +510,22 @@ namespace ShaderTools.Hlsl.Binding
             {
                 if (result.Selected == null)
                 {
-                    Diagnostics.ReportUndeclaredFunction(syntax, argumentTypes);
-                    return new BoundErrorExpression();
+                    if (result.Candidates.IsEmpty)
+                    {
+                        Diagnostics.ReportUndeclaredFunction(syntax, argumentTypes);
+                        return new BoundErrorExpression();
+                    }
+                    else
+                    {
+                        Diagnostics.ReportOverloadResolutionFailure(syntax, boundArguments.Length);
+                    }
                 }
-
-                var symbol1 = result.Selected.Signature.Symbol;
-                var symbol2 = result.Candidates.First(c => !c.Signature.Symbol.Equals(symbol1)).Signature.Symbol;
-                Diagnostics.ReportAmbiguousInvocation(syntax.SourceRange, symbol1, symbol2, argumentTypes);
+                else
+                {
+                    var symbol1 = result.Selected.Signature.Symbol;
+                    var symbol2 = result.Candidates.First(c => !c.Signature.Symbol.Equals(symbol1)).Signature.Symbol;
+                    Diagnostics.ReportAmbiguousInvocation(syntax.SourceRange, symbol1, symbol2, argumentTypes);
+                }
             }
 
             var convertedArguments = boundArguments.Select((a, i) => BindArgument(a, result, i, syntax.ArgumentList.Arguments[i].SourceRange)).ToImmutableArray();

src/ShaderTools/Hlsl/Diagnostics/DiagnosticExtensions.cs
@@ -152,6 +152,18 @@ namespace ShaderTools.Hlsl.Diagnostics
             diagnostics.Report(sourceRange, DiagnosticId.AmbiguousInvocation, symbol1, symbol2);
         }
 
+        public static void ReportOverloadResolutionFailure(this ICollection<Diagnostic> diagnostics, FunctionInvocationExpressionSyntax node, int argumentCount)
+        {
+            var name = node.Name.ToStringIgnoringMacroReferences();
+            diagnostics.Report(node.SourceRange, DiagnosticId.FunctionOverloadResolutionFailure, name, argumentCount);
+        }
+
+        public static void ReportOverloadResolutionFailure(this ICollection<Diagnostic> diagnostics, MethodInvocationExpressionSyntax node, int argumentCount)
+        {
+            var name = node.Name.ToStringIgnoringMacroReferences();
+            diagnostics.Report(node.SourceRange, DiagnosticId.MethodOverloadResolutionFailure, name, argumentCount);
+        }
+
         public static void ReportAmbiguousField(this ICollection<Diagnostic> diagnostics, SyntaxToken name)
         {
             diagnostics.Report(name.SourceRange, DiagnosticId.AmbiguousField, name.ValueText);

src/ShaderTools.Editor.VisualStudio/Hlsl/Navigation/GoToDefinitionProviders/SymbolReferenceGoToDefinitionProvider.cs
@@ -2,7 +2,6 @@ using System.Linq;
 using ShaderTools.Core.Text;
 using ShaderTools.Hlsl.Compilation;
 using ShaderTools.Hlsl.Syntax;
-using ShaderTools.Hlsl.Text;
 using ShaderTools.Editor.VisualStudio.Hlsl.SymbolSearch;
 using ShaderTools.Core.Syntax;
 

src/ShaderTools.Tests/Hlsl/Binding/ExpressionTestUtility.cs
@@ -31,6 +31,9 @@ namespace ShaderTools.Tests.Hlsl.Binding
                     return "#undeclared";
                 case DiagnosticId.CannotConvert:
                     return "#cannotconvert";
+                case DiagnosticId.FunctionOverloadResolutionFailure:
+                case DiagnosticId.MethodOverloadResolutionFailure:
+                    return "#novalidoverload";
                 default:
                     throw new ArgumentOutOfRangeException(nameof(diagnosticId));
             }

src/ShaderTools.Tests/Hlsl/Binding/FunctionDeclarationTests.cs
@@ -53,6 +53,33 @@ void main()
         }
 
         [Fact]
+        public void DetectsAmbiguousFunctionInvocation()
+        {
+            var code = @"
+void foo(int i) {}
+void foo(uint i) {}
+
+void main()
+{
+    foo(1.0);
+}";
+            AssertDiagnostics(code, DiagnosticId.AmbiguousInvocation);
+        }
+
+        [Fact]
+        public void DetectsOverloadResolutionFailure()
+        {
+            var code = @"
+void foo(int i) {}
+
+void main()
+{
+    foo();
+}";
+            AssertDiagnostics(code, DiagnosticId.FunctionOverloadResolutionFailure);
+        }
+
+        [Fact]
         public void AllowsFunctionDeclaredInMacro()
         {
             var code = @"

src/ShaderTools.Tests/Hlsl/Binding/FunctionInvocationExpressionTests.cs
@@ -27,7 +27,7 @@ namespace ShaderTools.Tests.Hlsl.Binding
         [InlineData("float3x3", "float3x3")]
         [InlineData("half2x3", "#ambiguous")]
         [InlineData("int2x2", "int")]
-        [InlineData("MyStruct", "#undeclared")]
+        [InlineData("MyStruct", "#novalidoverload")]
         public void TestFunctionOverloadResolution1Arg(string type, string expectedMatchType)
         {
             var code = $@"

src/ShaderTools/Hlsl/Binding/BoundNodes/BoundFunctionInvocationExpression.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using ShaderTools.Hlsl.Binding.Signatures;
 using ShaderTools.Hlsl.Symbols;
 using ShaderTools.Hlsl.Syntax;
@@ -18,7 +19,7 @@ namespace ShaderTools.Hlsl.Binding.BoundNodes
 
         public override TypeSymbol Type => Symbol == null ? TypeFacts.Unknown : Symbol.ReturnType;
 
-        public FunctionSymbol Symbol => Result.Selected?.Signature.Symbol;
+        public FunctionSymbol Symbol => Result.Selected?.Signature.Symbol ?? Result.Candidates.FirstOrDefault()?.Signature.Symbol;
 
         public FunctionInvocationExpressionSyntax Syntax { get; }
         public ImmutableArray<BoundExpression> Arguments { get; }

src/ShaderTools/Hlsl/Binding/BoundNodes/BoundMethodInvocationExpression.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using ShaderTools.Hlsl.Binding.Signatures;
 using ShaderTools.Hlsl.Symbols;
 using ShaderTools.Hlsl.Syntax;
@@ -19,7 +20,7 @@ namespace ShaderTools.Hlsl.Binding.BoundNodes
 
         public override TypeSymbol Type => Symbol == null ? TypeFacts.Unknown : Symbol.ReturnType;
 
-        public FunctionSymbol Symbol => Result.Selected?.Signature.Symbol;
+        public FunctionSymbol Symbol => Result.Selected?.Signature.Symbol ?? Result.Candidates.FirstOrDefault()?.Signature.Symbol;
 
         public MethodInvocationExpressionSyntax Syntax { get; }
         public BoundExpression Target { get; }

commit 3787722568c754a82dc441106ded4d80626fdda8

Fix numeric constructor overload resolution, broken in 9b2129048d6849840aea5dad5e755c94406dd98c

src/ShaderTools/Hlsl/Binding/Binder.Expressions.cs
@@ -408,13 +408,22 @@ namespace ShaderTools.Hlsl.Binding
             {
                 if (result.Selected == null)
                 {
-                    Diagnostics.ReportUndeclaredNumericConstructor(syntax, argumentTypes);
-                    return new BoundErrorExpression();
+                    if (result.Candidates.IsEmpty)
+                    {
+                        Diagnostics.ReportUndeclaredNumericConstructor(syntax, argumentTypes);
+                        return new BoundErrorExpression();
+                    }
+                    else
+                    {
+                        Diagnostics.ReportOverloadResolutionFailure(syntax, boundArguments.Length);
+                    }
                 }
-
-                var symbol1 = result.Selected.Signature.Symbol;
-                var symbol2 = result.Candidates.First(c => !c.Signature.Symbol.Equals(symbol1)).Signature.Symbol;
-                Diagnostics.ReportAmbiguousInvocation(syntax.SourceRange, symbol1, symbol2, argumentTypes);
+                else
+                {
+                    var symbol1 = result.Selected.Signature.Symbol;
+                    var symbol2 = result.Candidates.First(c => !c.Signature.Symbol.Equals(symbol1)).Signature.Symbol;
+                    Diagnostics.ReportAmbiguousInvocation(syntax.SourceRange, symbol1, symbol2, argumentTypes);
+                }
             }
 
             var convertedArguments = boundArguments.Select((a, i) => BindArgument(a, result, i, syntax.ArgumentList.Arguments[i].SourceRange)).ToImmutableArray();

src/ShaderTools/Hlsl/Binding/Binder.Lookup.cs
@@ -252,7 +252,7 @@ namespace ShaderTools.Hlsl.Binding
 
             var resolutionResult = OverloadResolution.Perform(signatures, argumentTypes);
 
-            if (type.Kind == SymbolKind.IntrinsicMatrixType && !resolutionResult.Candidates.Any())
+            if (type.Kind == SymbolKind.IntrinsicMatrixType && resolutionResult.Selected == null)
             {
                 // If no existing signatures for matrix constructor, then as long as we have the correct arguments, create a new function symbol on-the-fly.
                 // This is to handle the MANY overloads of matrix constructor functions, which are too many to create and store statically.

src/ShaderTools/Hlsl/Diagnostics/DiagnosticExtensions.cs
@@ -164,6 +164,12 @@ namespace ShaderTools.Hlsl.Diagnostics
             diagnostics.Report(node.SourceRange, DiagnosticId.MethodOverloadResolutionFailure, name, argumentCount);
         }
 
+        public static void ReportOverloadResolutionFailure(this ICollection<Diagnostic> diagnostics, NumericConstructorInvocationExpressionSyntax node, int argumentCount)
+        {
+            var name = node.Type.ToStringIgnoringMacroReferences();
+            diagnostics.Report(node.SourceRange, DiagnosticId.FunctionOverloadResolutionFailure, name, argumentCount);
+        }
+
         public static void ReportAmbiguousField(this ICollection<Diagnostic> diagnostics, SyntaxToken name)
         {
             diagnostics.Report(name.SourceRange, DiagnosticId.AmbiguousField, name.ValueText);

src/ShaderTools.Tests/Hlsl/Compilation/SemanticModelTests.cs
@@ -9,11 +9,19 @@ using ShaderTools.Testing;
 using ShaderTools.Testing.TestResources.Hlsl;
 using ShaderTools.Tests.Hlsl.Support;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ShaderTools.Tests.Hlsl.Compilation
 {
     public class SemanticModelTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public SemanticModelTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Theory]
         [HlslTestSuiteData]
         public void CanGetSemanticModel(string testFile)
@@ -30,7 +38,7 @@ namespace ShaderTools.Tests.Hlsl.Compilation
             Assert.NotNull(semanticModel);
 
             foreach (var diagnostic in semanticModel.GetDiagnostics())
-                Debug.WriteLine(diagnostic);
+                _output.WriteLine(diagnostic.ToString());
 
             Assert.Equal(0, semanticModel.GetDiagnostics().Count(x => x.Severity == DiagnosticSeverity.Error));
         }