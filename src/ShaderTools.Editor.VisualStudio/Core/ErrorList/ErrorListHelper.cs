tgjones/HlslTools

commit 0b3456bf04a2f2a215df92b29f92ce1a6c728efc

Speculative fix for #73.

src/ShaderTools.Editor.VisualStudio/Core/ErrorList/ErrorListHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
 using ShaderTools.Core.Diagnostics;
@@ -10,7 +12,6 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
 {
     internal sealed class ErrorListHelper : IErrorListHelper
     {
-        private readonly object _lockObject = new object();
         private readonly ErrorListProvider _errorListProvider;
         private readonly IServiceProvider _serviceProvider;
         private readonly ITextDocument _textDocument;
@@ -25,7 +26,7 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
 
         public void AddError(Diagnostic diagnostic, TextSpan span)
         {
-            lock (_lockObject)
+            Application.Current.Dispatcher.Invoke(() =>
             {
                 if (_disposed)
                     return;
@@ -52,7 +53,7 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
                 task.Navigate += OnTaskNavigate;
 
                 _errorListProvider.Tasks.Add(task);
-            }
+            }, DispatcherPriority.Background);
         }
 
         private void OnTaskNavigate(object sender, EventArgs e)
@@ -63,17 +64,17 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
 
         public void Clear()
         {
-            lock (_lockObject)
+            Application.Current.Dispatcher.Invoke(() =>
             {
                 if (_disposed)
                     return;
                 _errorListProvider.Tasks.Clear();
-            }
+            }, DispatcherPriority.Background);
         }
 
         void IDisposable.Dispose()
         {
-            lock (_lockObject)
+            Application.Current.Dispatcher.Invoke(() =>
             {
                 if (_errorListProvider != null)
                 {
@@ -81,7 +82,7 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
                     _errorListProvider.Dispose();
                 }
                 _disposed = true;
-            }
+            }, DispatcherPriority.Background);
         }
     }
 }

commit dd5f885874c3fdd651cb8bcdc2434f23aac9f61d

Fix error when opening files that have existing syntax errors.
(broken since 0b3456bf04a2f2a215df92b29f92ce1a6c728efc)

src/ShaderTools.Editor.VisualStudio/Core/ErrorList/ErrorListHelper.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Threading;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Threading;
 using ShaderTools.Core.Diagnostics;
 using ShaderTools.Core.Text;
 using ShaderTools.Editor.VisualStudio.Core.Navigation;
@@ -26,14 +27,14 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
 
         public void AddError(Diagnostic diagnostic, TextSpan span)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            ThreadHelper.JoinableTaskFactory.Run(() =>
             {
                 if (_disposed)
-                    return;
+                    return TplExtensions.CompletedTask;
 
                 var sourceText = span.SourceText as VisualStudioSourceText;
                 if (sourceText == null)
-                    return;
+                    return TplExtensions.CompletedTask;
 
                 var line = sourceText.Snapshot.GetLineFromPosition(span.Start);
 
@@ -53,7 +54,9 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
                 task.Navigate += OnTaskNavigate;
 
                 _errorListProvider.Tasks.Add(task);
-            }, DispatcherPriority.Background);
+
+                return TplExtensions.CompletedTask;
+            });
         }
 
         private void OnTaskNavigate(object sender, EventArgs e)
@@ -64,17 +67,18 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
 
         public void Clear()
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            ThreadHelper.JoinableTaskFactory.Run(() =>
             {
                 if (_disposed)
-                    return;
+                    return TplExtensions.CompletedTask;
                 _errorListProvider.Tasks.Clear();
-            }, DispatcherPriority.Background);
+                return TplExtensions.CompletedTask;
+            });
         }
 
         void IDisposable.Dispose()
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            ThreadHelper.JoinableTaskFactory.Run(() =>
             {
                 if (_errorListProvider != null)
                 {
@@ -82,7 +86,8 @@ namespace ShaderTools.Editor.VisualStudio.Core.ErrorList
                     _errorListProvider.Dispose();
                 }
                 _disposed = true;
-            }, DispatcherPriority.Background);
+                return TplExtensions.CompletedTask;
+            });
         }
     }
 }