f4325a2e5c46b8ce542d19fbf9b3b28b097757dd
Tim Jones <tim@timjones.io>
2017-02-12 22:58:57 +0800

    Rename ShaderTools.Editor -> ShaderTools.EditorServices

--- src/ShaderTools.EditorServices/Properties/AssemblyInfo.cs
+++ src/ShaderTools.EditorServices/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ShaderTools.Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ShaderTools.Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
