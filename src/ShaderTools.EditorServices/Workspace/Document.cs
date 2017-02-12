58d8e6f4b102b214bb8caeecbda7e01540990245
Tim Jones <tim@timjones.io>
2017-02-12 23:27:43 +0800

    Add Document class, implemented using Roslyn's AsyncLazy

---
+++ src/ShaderTools.EditorServices/Workspace/Document.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ShaderTools.Core.Syntax;
+using ShaderTools.Core.Text;
+using ShaderTools.EditorServices.Utility;
+
+namespace ShaderTools.EditorServices.Workspace
+{
+    public sealed class Document
+    {
+        private readonly SourceText _sourceText;
+        private readonly Func<SourceText, CancellationToken, SyntaxTreeBase> _compileFunc;
+        private readonly AsyncLazy<SyntaxTreeBase> _lazySyntaxTree;
+
+        public Document(SourceText sourceText, Func<SourceText, CancellationToken, SyntaxTreeBase> compileFunc)
+        {
+            _sourceText = sourceText;
+
+            _lazySyntaxTree = new AsyncLazy<SyntaxTreeBase>(ct => Task.Run(() => compileFunc(sourceText, ct), ct), true);
+        }
+
+        public Task<SyntaxTreeBase> GetSyntaxTreeAsync(CancellationToken cancellationToken)
+        {
+            return _lazySyntaxTree.GetValueAsync(cancellationToken);
+        }
+
+        public Document WithSourceText(SourceText sourceText)
+        {
+            return new Document(sourceText, _compileFunc);
+        }
+    }
+}
