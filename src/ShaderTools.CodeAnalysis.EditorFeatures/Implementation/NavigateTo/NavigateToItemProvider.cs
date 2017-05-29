f4eeaed1a2986fc76922dcd54c85eb117cb9de2c
Tim Jones <tim@timjones.io>
2017-05-29 19:00:22 +0800

    Missing file from previous commit

---
+++ src/ShaderTools.CodeAnalysis.EditorFeatures/Implementation/NavigateTo/NavigateToItemProvider.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Language.NavigateTo.Interfaces;
+using ShaderTools.CodeAnalysis.Shared.TestHooks;
+using ShaderTools.Utilities.Diagnostics;
+
+namespace ShaderTools.CodeAnalysis.Editor.Implementation.NavigateTo
+{
+    internal partial class NavigateToItemProvider : INavigateToItemProvider
+    {
+        private readonly Workspace _workspace;
+        private readonly IAsynchronousOperationListener _asyncListener;
+        private readonly INavigateToItemDisplayFactory _displayFactory;
+
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public NavigateToItemProvider(
+            Workspace workspace,
+            IGlyphService glyphService,
+            IAsynchronousOperationListener asyncListener)
+        {
+            Contract.ThrowIfNull(workspace);
+            Contract.ThrowIfNull(glyphService);
+            Contract.ThrowIfNull(asyncListener);
+
+            _workspace = workspace;
+            _asyncListener = asyncListener;
+
+            _displayFactory = new NavigateToItemDisplayFactory(new NavigateToIconFactory(glyphService));
+        }
+
+        public void StopSearch()
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void Dispose()
+        {
+            this.StopSearch();
+            (_displayFactory as IDisposable)?.Dispose();
+        }
+
+        public void StartSearch(INavigateToCallback callback, string searchValue)
+        {
+            this.StopSearch();
+
+            if (string.IsNullOrWhiteSpace(searchValue))
+            {
+                callback.Done();
+                return;
+            }
+
+            var searchCurrentDocument = GetSearchCurrentDocumentOption(callback);
+            var searcher = new Searcher(
+                _workspace,
+                _asyncListener,
+                _displayFactory,
+                callback,
+                searchValue,
+                searchCurrentDocument,
+                _cancellationTokenSource.Token);
+
+            searcher.Search();
+        }
+
+        private bool GetSearchCurrentDocumentOption(INavigateToCallback callback)
+        {
+            try
+            {
+                return GetSearchCurrentDocumentOptionWorker(callback);
+            }
+            catch (TypeLoadException)
+            {
+                // The version of the APIs we call in VS may not match what the 
+                // user currently has on the box (as the APIs have changed during
+                // the VS15 timeframe.  Be resilient to this happening and just
+                // default to searching all documents.
+                return false;
+            }
+        }
+
+        private bool GetSearchCurrentDocumentOptionWorker(INavigateToCallback callback)
+        {
+            // INavigateToOptions2, in VS2017, has a SearchCurrentDocument property.
+            var navigateToOptions = callback.Options;
+
+            try
+            {
+                var interfaceType = navigateToOptions.GetType().GetInterface("INavigateToOptions2");
+                if (interfaceType != null)
+                {
+                    var searchCurrentDocumentProperty = interfaceType.GetProperty("SearchCurrentDocument");
+                    if (searchCurrentDocumentProperty != null)
+                    {
+                        return (bool) searchCurrentDocumentProperty.GetValue(navigateToOptions);
+                    }
+                }
+
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
