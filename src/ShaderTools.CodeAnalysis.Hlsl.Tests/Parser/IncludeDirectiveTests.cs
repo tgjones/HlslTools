b5eed9f8b1e0701656f29bbe3b5d47da20347e76
Julien Vulliet <julien@mrvux.com>
2018-11-20 16:38:03 +0100

    As per #130
    Make source file constructor public (otherwise it's no possible to ever provide a custom file resolver outside of the assembly itself)
    
    Add test for include directive (checks that tokens are found and that they have correct settings)
    Add test to use a custom file resolver (verifies that it will be called)

---
+++ src/ShaderTools.CodeAnalysis.Hlsl.Tests/Parser/IncludeDirectiveTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ShaderTools.CodeAnalysis.Hlsl.Syntax;
+using ShaderTools.CodeAnalysis.Text;
+using Xunit;
+
+namespace ShaderTools.CodeAnalysis.Hlsl.Tests.Parser
+{
+    public class IncludeDirectiveTests
+    {
+        [Theory]
+        [InlineData("#include\"foo.fxh\"", true, "foo.fxh")]
+        [InlineData("#include<foo.fxh>", false, "foo.fxh")]
+        public void TestName(string text, bool isLocal, string trimmedName)
+        {
+            var expression = SyntaxFactory.ParseSyntaxTree(SourceText.From(text));
+            var trivia = ((SyntaxToken)expression.Root.ChildNodes[0]).LeadingTrivia;
+
+            Assert.Equal(1, trivia.Length);
+                
+            var includeTrivia = (IncludeDirectiveTriviaSyntax)trivia[0];
+
+            Assert.NotNull(expression);
+            Assert.Equal(SyntaxKind.IncludeDirectiveTrivia, includeTrivia.Kind);
+
+            IncludeDirectiveTriviaSyntax directive = (IncludeDirectiveTriviaSyntax)includeTrivia;
+
+            Assert.Equal(isLocal, directive.IsLocal);
+            Assert.Equal(trimmedName, directive.TrimmedFilename);
+        }
+    }
+}
