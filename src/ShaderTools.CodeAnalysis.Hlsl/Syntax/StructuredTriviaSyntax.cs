75d206222ea38fe97bdba7f90e09e0badac0994e
Jesse Gielen <jessegielen@hotmail.com>
2018-11-24 01:16:48 +0100

    StructureTriviaSyntax

--- src/ShaderTools.CodeAnalysis.Hlsl/Syntax/StructuredTriviaSyntax.cs
+++ src/ShaderTools.CodeAnalysis.Hlsl/Syntax/StructuredTriviaSyntax.cs
@@ -3,18 +3,8 @@ using ShaderTools.CodeAnalysis.Diagnostics;
 
 namespace ShaderTools.CodeAnalysis.Hlsl.Syntax
 {
-    public abstract class StructuredTriviaSyntax : SyntaxNode
+    public abstract partial class StructuredTriviaSyntax : SyntaxNode
     {
         internal override bool IsStructuredTrivia => true;
-
-        protected StructuredTriviaSyntax(SyntaxKind kind, IEnumerable<Diagnostic> diagnostics)
-            : base(kind, diagnostics)
-        {
-        }
-
-        protected StructuredTriviaSyntax(SyntaxKind kind)
-            : base(kind)
-        {
-        }
     }
 }
