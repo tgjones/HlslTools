d707f8436c2bac2dd1cb80cf75f577c80fe0d212
Tim Jones <tim@timjones.io>
2019-12-17 23:41:52 +0000

    Support F1 context help. Fixes #158.

---
+++ src/ShaderTools.VisualStudio.LanguageServices.Hlsl/LanguageService/HlslHelpContextService.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Text;
+using ShaderTools.CodeAnalysis;
+using ShaderTools.CodeAnalysis.Hlsl.Syntax;
+using ShaderTools.CodeAnalysis.Shared.Extensions;
+using ShaderTools.CodeAnalysis.Syntax;
+using ShaderTools.VisualStudio.LanguageServices.Implementation.F1Help;
+
+namespace ShaderTools.VisualStudio.LanguageServices.Hlsl.LanguageService
+{
+    [ExportLanguageService(typeof(IHelpContextService), LanguageNames.Hlsl)]
+    internal sealed class HlslHelpContextService : AbstractHelpContextService
+    {
+        public override string Language => "hlsl";
+
+        public override string Product => "hlsl";
+
+        public override async Task<string> GetHelpTermAsync(Document document, TextSpan span, CancellationToken cancellationToken)
+        {
+            // Find the token under the start of the selection.
+            var syntaxTree = await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
+            var rootFileRange = syntaxTree.MapRootFileRange(span);
+            var token = await syntaxTree.GetTouchingTokenAsync(rootFileRange.Start, cancellationToken, findInsideTrivia: true).ConfigureAwait(false);
+
+            if (IsValid(token, span))
+            {
+                return ((SyntaxToken)token).Text;
+            }
+
+            return string.Empty;
+        }
+
+        private static bool IsValid(ISyntaxToken token, TextSpan span)
+        {
+            // If the token doesn't actually intersect with our position, give up
+            return token.FileSpan.IsInRootFile && token.FileSpan.Span.IntersectsWith(span);
+        }
+    }
+}
---
+++ src/ShaderTools.VisualStudio.LanguageServices/Implementation/F1Help/AbstractHelpContextService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Text;
+using ShaderTools.CodeAnalysis;
+
+namespace ShaderTools.VisualStudio.LanguageServices.Implementation.F1Help
+{
+    internal abstract class AbstractHelpContextService : IHelpContextService
+    {
+        public abstract string Language { get; }
+        public abstract string Product { get; }
+
+        public abstract Task<string> GetHelpTermAsync(Document document, TextSpan textSpan, CancellationToken cancellationToken);
+    }
+}
